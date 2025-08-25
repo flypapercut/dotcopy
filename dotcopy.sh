91e2484c15ac2662b16fd877f2c80861cc022d9c
flypapercut <flypapercut@proton.me>
2025-08-24 21:32:11 -0300

    add mapping file argument

--- dotcopy.sh
+++ dotcopy.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/bash
 
+
+
 capture_flag() {
- case "$@" in
-  -to-repo) echo "..."; main "-to-repo";;
-	-to-system) echo "..."; main "-to-system";;
-   *) echo "could not identify flag";; 
- esac
+
+file_flag=;
+   
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+		-to-repo) echo ""; shift; mode_flag="-to-repo";;
+		-to-system) echo ""; shift; mode_flag="-to-system";;
+    -f)
+      if [[ -n "${2:-}" ]]; then
+        file_flag="$2"
+				echo $file_flag
+				main $mode_flag
+      	shift 2
+      else
+        echo "Error: -f require an argument." >&2
+          exit 1
+      fi
+      ;;
+    *) shift;;
+  esac
+done
+
+if [[ $# -eq 0 && -z $file_flag ]] then
+	echo "could not identify flags"
+fi
 }
+
 
 copy_file() {
 	if [[ $3 == "-to-repo" ]]; then
@@ -20,16 +43,16 @@ copy_file() {
 
 
 iterate() {
-	flag=$1
+	mode_flag=$1
 	while IFS=$'\t' read -r repo_path system_path; do
 
 		repo_path=$(echo $repo_path | envsubst)
 		system_path=$(echo $system_path | envsubst)
-		copy_file $repo_path $system_path $flag
+		copy_file $repo_path $system_path $mode_flag
 
 	done < <(jq -r '.files
 			| to_entries[]
-			| "\(.value.repo_path)\t\(.value.system_path)"' mapping.json)
+			| "\(.value.repo_path)\t\(.value.system_path)"' $file_flag)
 }
 
 
